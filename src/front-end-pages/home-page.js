ef4663329bdd61d58797c216504e9d32045fa267
Sandeep Kumar Reddy Menakuru <sandeepkumarreddymenakuru@Sandeeps-MacBook-Air.local>
2021-11-12 10:58:57 +0000

    removed all default files

---
+++ src/front-end-pages/home-page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "../components/logo";
+
+const HomePage =()=>{
+
+    return (
+        <div>
+            <Logo />
+        </div>
+    )
+}
+
+export default HomePage
