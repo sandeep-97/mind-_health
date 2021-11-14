3c76a41e8c6555ba56835e177b007d9bc3b4a17d
Sandeep Kumar Reddy Menakuru <sandeepkumarreddymenakuru@Sandeeps-MacBook-Air.local>
2021-11-14 17:31:52 +0000

    started working on nav bar getting error on href

---
+++ src/components/nav-bar_components/information.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Information = () => {
+    return (
+        <div style={{backgroundColor:'red',height:'100em'}}>
+            
+        </div>
+    )
+}
+
+export default Information
--- src/front-end-pages/home-page.js
+++ src/front-end-pages/home-page.js
@@ -1,10 +1,78 @@
-import React from "react";
+import React,{useState} from "react";
 import Donate from "../components/donate-button-link";
 import GetHelp from "../components/help-button-link";
 import Logo from "../components/logo";
+import Information from "../components/nav-bar_components/information";
 import '../style/home-page.scss'
 
 const HomePage =()=>{
+
+    const [open,setOpen]=useState({
+        informationNav:false,
+        aboutNav:false,
+        newsNav:false,
+        involveNav:false,
+        consultNav:false
+    })
+    console.log('hi')
+    const monitoringNavState=(navSate)=>{
+        console.log(1)
+        switch(navSate){
+            case 'information':
+                setOpen({
+                    ...open,
+                    informationNav:!open.informationNav,
+                    aboutNav:false,
+                    newsNav:false,
+                    involveNav:false,
+                    consultNav:false
+                })
+                break
+            case 'about':
+                setOpen({
+                    ...open,
+                    aboutNav:!open.aboutNav,
+                    informationNav:false,
+                    newsNav:false,
+                    involveNav:false,
+                    consultNav:false
+                })
+                break
+            case 'news':
+                setOpen({
+                    ...open,
+                    newsNav:!open.newsNav,
+                    informationNav:false,
+                    aboutNav:false,
+                    involveNav:false,
+                    consultNav:false
+                })
+                break
+            case 'involve':
+                setOpen({
+                    ...open,
+                    involveNav:!open.involveNav,
+                    informationNav:false,
+                    aboutNav:false,
+                    newsNav:false,
+                    consultNav:false
+                })
+                break
+            case 'consult':
+                setOpen({
+                    ...open,
+                    consultNav:!open.consultNav,
+                    informationNav:false,
+                    aboutNav:false,
+                    newsNav:false,
+                    involveNav:false
+                })
+                break
+            default:
+                setOpen(open)
+        }
+        console.log(open.informationNav)
+    }
 
     return (
         <div>
@@ -23,19 +91,30 @@ const HomePage =()=>{
             <section className='nav_section'>
                 <ul className='nav_list'>
                     <li> 
-                        information
+                        <a href='a' onClick={()=>{monitoringNavState('information')}} >
+                            information
+                        </a>
+                        {open.informationNav?<Information />:null}
                     </li>
                     <li>
-                        about us
+                        <a href='/' onClick={()=>monitoringNavState('about')} >
+                            about us
+                        </a>
                     </li>
                     <li>
-                        news
+                        <a href='/' onClick={()=>monitoringNavState('news')}>
+                            news
+                        </a>
                     </li>
                     <li>
-                        get involve
+                        <a href='/' onClick={()=>monitoringNavState('involve')}>
+                            get involve
+                        </a>
                     </li>
                     <li>
-                        shop
+                        <a href='/' onClick={()=>monitoringNavState('consult')}>
+                            consult
+                        </a>
                     </li>
                 </ul>
             </section>
